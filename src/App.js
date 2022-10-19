7afc876d78bfc936b4566bc2edfef93428a8c316
Kazi Md Minhajul Haider <kaziminhajulhaider@gmail.com>
2022-10-19 14:47:14 +0600

    fresh commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import React from "react";
+import { useMemo, useRef, useState } from 'react';
 
 function App() {
-  return <div>App</div>
+    const [items, setItems] = useState([]);
+    const [query, setQuery] = useState('');
+    const inputRef = useRef();
+
+    const filteredItems = useMemo(
+        () => items.filter((item) => item.toLowerCase().includes(query.toLowerCase())),
+        [items, query]
+    );
+
+    function onSubmit(e) {
+        e.preventDefault();
+
+        const { value } = inputRef.current;
+        if (value === '') return;
+        setItems((prev) => [...prev, value]);
+
+        inputRef.current.value = '';
+    }
+
+    return (
+        <>
+            Search:
+            <input value={query} onChange={(e) => setQuery(e.target.value)} type="search" />
+            <br />
+            <br />
+            <form onSubmit={onSubmit}>
+                New Item: <input ref={inputRef} type="text" />
+                <button type="submit">Add</button>
+            </form>
+            <h3>Items:</h3>
+            {filteredItems.map((item) => (
+                <div>{item}</div>
+            ))}
+        </>
+    );
 }
 
 export default App;
